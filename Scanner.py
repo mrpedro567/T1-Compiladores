e988abebc6de72c2c8b74eb4d278188fc2b702cc
Pedro Silva <mrgpedrosilva@gmail.com>
2023-11-07 15:07:38 -0300

    scanner

--- Scanner.py
+++ Scanner.py
@@ -1,5 +1,6 @@
 import variables as var
 from tokenClass import Token
+from symbolTable import SymbolTable
 
 def nextChar(file): 
     if(var.getColumn() == len(file[var.getLine()])):
@@ -8,11 +9,11 @@ def nextChar(file):
     else: 
         var.setColumn(var.getColumn() + 1)
 
-def scanner(file):
+def scanner(file, table):
     letters = list('ABCDEFGHIJKLMNOPKRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
     digits = list('0123456789')
     state = 0
-    word = []
+    word = str("")
     i = 1
 
     while(i == 1): 
@@ -22,134 +23,293 @@ def scanner(file):
 
                 if(char in letters):
                     state = 6 
-                    word.append(char)
+                    word += char
                     continue
 
                 if(char in digits):
                     state = 1
-                    word.append(char)
+                    word += char
                     continue
 
                 match(char):
                     case('"'):
                         state = 4  
-                        word.append(char)
+                        word += char
                     case('{'):
                         state = 7
-                        word.append(char)
+                        word += char
                     case('<'): 
                         state = 9
-                        word.append(char)
+                        word += char
                     case('>'):
                         state = 13
-                        word.append(char)
+                        word += char
                     case('='):
                         state = 15
-                        word.append(char)
+                        word += char
                     case('+'):
                         state = 16
-                        word.append(char)
+                        word += char
                     case('-'):
                         state = 16
-                        word.append(char)
+                        word += char
                     case('*'):
                         state = 16
-                        word.append(char)
+                        word += char
                     case('/'):
                         state = 16
-                        word.append(char)
+                        word += char
                     case('('):
                         state = 18
-                        word.append(char)
+                        word += char
                     case(')'):
                         state = 17
-                        word.append(char)
+                        word += char
                     case(';'):
                         state = 19
-                        word.append(char)
+                        word += char
                     case(','):
                         state = 20
-                        word.append(char)
+                        word += char
                     case(' '):
                         state = 21
-                        word.append(char)
+                        word += char
                     case('\t'):
                         state = 22
-                        word.append(char)
+                        word += char
                     case('\n'):
                         state = 23
-                        word.append(char)
+                        word += char
                     case('$'): 
                         state = 24
-                        word.append(char)
+                        word += char
                     case(_):
                         print("Erro: Caractere invalido na linha: " + var.getLine() + " , coluna: " + var.getColumn())
                         i = 0
-                        break;
+                        break
             case(1):
-                return [word, state]
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                if(char in digits):
+                    state = 2
+                    word += char
+                    continue
+                elif(char == "E"):
+                        state = 26
+                        word += char
+                elif(char == "e"):
+                        state = 26
+                        word += char
+                elif(char == "."):
+                        state = 3
+                        word += char
+                else:
+                    nextChar(file)
+                    state = 0
+                    return Token("Num", word, "inteiro")
             case(2):
-                return [word, state]
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                if(char in digits):
+                    state = 2
+                    word += char
+                elif(char == "."):
+                    state = 3
+                    word += char
+                else:
+                    nextChar(file)
+                    state = 0
+                    return Token("Num", word, "inteiro")
             case(3):
-                return [word, state]
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                if(char in digits):
+                    state = 25
+                    word += char
+                else:
+                    print('Erro: Esperado caráctere: ". Linha: ' + var.getLine() + ' Coluna: ' + var.getColumn())
             case(4):
                 nextChar(file)
                 char = file[var.getLine()][var.getColumn()]
-
                 match(char):
                     case('"'):
                         state = 5
-                        word.append(char)
+                        word += char
                     case('$'):
                         print('Erro: Esperado caráctere: ". Linha: ' + var.getLine() + ' Coluna: ' + var.getColumn())
                     case(_):
                         state = 4
-                        word.append(char)
+                        word += char
             case(5):
-                return [word, state]
+                nextChar(file)
+                token = table.get(word)
+                if(token == None):
+                    token = Token("Lit", word, "literal")
+                    token = table.insert(word, token)
+                    return token
+                else:
+                    return token
             case(6):
-                return [word, state]
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                if(char in letters):
+                    state = 6 
+                    word += char
+                    continue
+                elif(char in digits):
+                    state = 6 
+                    word += char
+                    continue
+                elif (char == '_'):
+                    state = 6 
+                    word += char
+                    continue
+                else:
+                    token = table.get(word)
+                    if(token == None):
+                        token = Token("id", word, None)
+                        token = table.insert(word, token)
+                        return token
+                    else:
+                        return token
             case(7):
-                return [word, state]
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                match(char):
+                    case('}'):
+                        state = 8  
+                        word += char    
+                    case('$'):
+                        print('Erro: Esperado caráctere: ". Linha: ' + var.getLine() + ' Coluna: ' + var.getColumn())
+                    case(_):
+                        state = 7
+                        word += char
             case(8):
-                return [word, state]
+                state = 0
+                word = str("")
+                nextChar(file)
+                continue   
             case(9):
-                return [word, state]
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                if(char == "="):
+                    state = 10
+                    word += char
+                elif(char == "-"):
+                    state = 11
+                    word += char
+                elif(char == ">"):
+                    state = 12
+                    word += char
+                else:
+                    nextChar(file)
+                    state = 0
+                    return Token("OPR", word, None)
             case(10):
-                return [word, state]
+                nextChar(file)
+                state = 0
+                return Token("OPR", word, None)
             case(11):
-                return [word, state]
+                nextChar(file)
+                state = 0
+                return Token("RCB", word, None)
             case(12):
-                return [word, state]
+                nextChar(file)
+                state = 0
+                return Token("OPR", word, None)
             case(13):
-                return [word, state]
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                if(char == "="):
+                    state = 14
+                    word += char
+                else:
+                    nextChar(file)
+                    state = 0
+                    return Token("OPR", word, None)
             case(14):
-                return [word, state]
+                nextChar(file)
+                state = 0
+                return Token("OPR", word, None)
             case(15):
-                return [word, state]
+                nextChar(file)
+                state = 0
+                return Token("OPR", word, None)
             case(16):
                 nextChar(file)
                 state = 0
                 return Token("OPM", word, None)
             case(17):
-                return [word, state]
+                nextChar(file)
+                state = 0
+                return Token("FC_P", word, None)
             case(18):
-                return [word, state]
+                nextChar(file)
+                state = 0
+                return Token("AB_P", word, None)
             case(19):
-                return [word, state]
+                nextChar(file)
+                state = 0
+                return Token("PT_V", word, None)
             case(20):
-                return [word, state]
+                nextChar(file)
+                state = 0
+                return Token("VIR", word, None)
             case(21):
-                return [word, state]
+                state = 0
+                word = str("")
+                nextChar(file)
+                continue   
             case(22):
-                return [word, state]
+                state = 0
+                word = str("")
+                nextChar(file)
+                continue   
             case(23):
-                return [word, state]
+                state = 0
+                word = str("")
+                nextChar(file)
+                continue   
             case(24):
-                return [word, state]
+               #EOF
             case(25):
-                return [word, state]
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                if(char in digits):
+                    state = 25
+                    word += char
+                elif(char == "E"):
+                    state = 26
+                    word += char
+                elif(char == "e"):
+                    state = 26
+                    word += char
+                else
+                    nextChar(file)
+                    state = 0
+                    return Token("Num", word, "real")
             case(26):
-                return [word, state]
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                if(char in digits):
+                    state = 28
+                    word += char
+                elif (char == "+" or char == "-"):
+                    state = 27
+                    word += char
+                else:
+                    print('Erro: Esperado caráctere: ". Linha: ' + var.getLine() + ' Coluna: ' + var.getColumn())
+            case(27):
+                nextChar(file)
+                char = file[var.getLine()][var.getColumn()]
+                if(char in digits):
+                    state = 28
+                    word += char
+                else:
+                    print('Erro: Esperado caráctere: ". Linha: ' + var.getLine() + ' Coluna: ' + var.getColumn())
+            case(28):
+                nextChar(file)
+                state = 0
+                return Token("Num", word, "real")
             case _:
                 return [word, state]
