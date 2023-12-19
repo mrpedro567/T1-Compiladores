f8462c3493abeac3ba40fbb8e7b5db57ba463e93
Pedro Silva <mrgpedrosilva@gmail.com>
2023-12-19 00:04:28 -0300

    Phrase level recovery

--- parser/parser.py
+++ parser/parser.py
@@ -1,5 +1,6 @@
 from scanner.Scanner import scanner
 from collections import deque
+from scanner.tokenClass import Token
 import scanner.variables as var
 import parser.productions as prod
 import pandas as pd
@@ -13,14 +14,102 @@ def parse(lines, symbolTable):
     gotoTable = pd.read_csv('parser/table/goto.csv', index_col=0)
     token = scanner(lines, symbolTable)
 
+
     while(True): 
         state = stack[-1]
         action = actionTable.iloc[state][token.getClasse()]
         
         if(isinstance(action, str) == False):
-            print("Erro sintático na linha " + str(var.line) + " e coluna " + str(var.column), "Posição invalida para:", token.getLexema())
-            token = scanner(lines, symbolTable)
-            continue
+            
+
+            match(state): 
+                case 0: 
+                    print("Erro sintático na linha " + str(var.line + 1) + " e coluna " + str(var.column + 1), "Esperado: Inicio", "Recebido: ", token.getLexema())
+                    
+                    token = Token("inicio", "inicio", "inicio")
+                    
+                    var.line = var.lastToken[0]
+                    var.column = var.lastToken[1]
+                    
+                    continue
+                case 2: 
+                    print("Erro sintático na linha " + str(var.line + 1) + " e coluna " + str(var.column + 1), "Esperado: varinicio", "Recebido: ", token.getLexema())
+                    
+                    token = Token("varinicio", "varinicio", "varinicio")
+                    
+                    var.line = var.lastToken[0]
+                    var.column = var.lastToken[1]
+                    
+                    continue
+                case 11: 
+                    print("Erro sintático na linha " + str(var.line + 1) + " e coluna " + str(var.column + 1), "Esperado: id, Recebido: ",  token.getLexema())
+
+                    token = scanner(lines, symbolTable)
+                    continue
+
+                case 13: 
+                    print("Erro sintático na linha " + str(var.line + 1) + " e coluna " + str(var.column + 1), "Esperado: atribuição", "Recebido: ", token.getLexema())
+                    
+                    token = Token("rcb", "<-", None)
+                    
+                    var.line = var.lastToken[0]
+                    var.column = var.lastToken[1]
+                    
+                    continue
+                
+                case 16: 
+                    print("Erro sintático na linha " + str(var.line + 1) + " e coluna " + str(var.column + 1), "Esperado: (", "Recebido: ", token.getLexema())
+                    
+                    token = Token("ab_p", "(", None)
+                    
+                    var.line = var.lastToken[0]
+                    var.column = var.lastToken[1]
+                    
+                    continue
+
+                case 17: 
+                    print("Erro sintático na linha " + str(var.line + 1) + " e coluna " + str(var.column + 1), "Esperado: (", "Recebido: ", token.getLexema())
+                    
+                    token = Token("ab_p", "(", None)
+                    
+                    var.line = var.lastToken[0]
+                    var.column = var.lastToken[1]
+                    
+                    continue
+
+                case 20: 
+                    print("Erro sintático na linha " + str(var.line + 1) + " e coluna " + str(var.column + 1), "Esperado: ;", "Recebido: ", token.getLexema())
+                    
+                    token = Token("pt_v", ";", None)
+                    
+                    var.line = var.lastToken[0]
+                    var.column = var.lastToken[1]
+                    
+                    continue
+                case 29: 
+                    print("Erro sintático na linha " + str(var.line + 1) + " e coluna " + str(var.column + 1), "Esperado: ;", "Recebido: ", token.getLexema())
+                    
+                    token = Token("pt_v", ";", None)
+                    
+                    var.line = var.lastToken[0]
+                    var.column = var.lastToken[1]
+                    
+                    continue
+                case 30: 
+                    print("Erro sintático na linha " + str(var.line + 1) + " e coluna " + str(var.column + 1), "Esperado: ;", "Recebido: ", token.getLexema())
+                    
+                    token = Token("pt_v", ";", None)
+                    
+                    var.line = var.lastToken[0]
+                    var.column = var.lastToken[1]
+                    
+                    continue
+                case _:  
+                    print("Erro sintático na linha " + str(var.line) + " e coluna " + str(var.column), "Posição invalida para:", token.getLexema())
+                    token = scanner(lines, symbolTable)
+                    continue
+        else: 
+            var.lastToken = [var.line, var.column]
 
         if action[0] == 's':
             stack.append(int(action[1:]))
@@ -39,6 +128,3 @@ def parse(lines, symbolTable):
             print(production[0])
         elif action == 'acc':
             break
-        else:
-            # erro 
-            print("Erro sintático na linha " + str(var.line) + " e coluna " + str(var.column))
--- scanner/variables.py
+++ scanner/variables.py
@@ -1,5 +1,8 @@
 def init(): 
     global line 
     global column
+    global lastToken 
+
+    lastToken = [0,0]
     line = 0
     column = 0
